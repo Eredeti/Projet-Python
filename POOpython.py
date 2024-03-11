d86cd89b42e89d1ab213a4d09da932e8a220730f
Eredeti <93719013+Eredeti@users.noreply.github.com>
2024-03-11 20:03:21 +0100

    Update POOpython.py

--- POOpython.py
+++ POOpython.py
@@ -2,35 +2,48 @@ import requests
 import pandas as pd
 import folium
 
-class Donnees:
-    def __init__(self, key, don1, don2):
-        self.key = str(key)
-        self.don1 = int(don1)
-        self.don2 = int(don2)
+class Esport:
+    def __init__(self,annee,classement):
+        self.annee = str(annee)
+        self.classement = int(classement)
     
     def __str__(self):
-        return f"Clé: {self.key}, Coût: {self.don1}, Nb joueur : {self.don2}"
+        pass 
     
     def ratio(self):
-        # Récupération des données géographiques des États-Unis depuis GitHub
+        # Récupération des données géographiques des pays depuis Github
         state_geo = requests.get(
             "https://raw.githubusercontent.com/Eredeti/Projet-Python/main/world_countries.json"
         ).json()
 
+        url = f"https://raw.githubusercontent.com/Eredeti/Projet-Python/main/Top_country_{self.annee}.csv"
+        
         # Lecture des données à partir du fichier CSV sur GitHub
-        state_data = pd.read_csv("https://raw.githubusercontent.com/Eredeti/Projet-Python/main/Top_country.csv")
+        state_data = pd.read_csv(url)
 
         # Calcul du ratio d'investissement par joueur
         state_data["Ratio"] = state_data["Gain_Earn"]/state_data["Players"]
 
         # Création de la carte Folium
-        m = folium.Map(location=[48, -102], zoom_start=1)
+        m = folium.Map(location=[47, 2], zoom_start=2)
 
+        # Sélection des 10 premiers du top ratio
+        top = state_data.head(self.classement)
+
+        # Formatage des données dans une chaîne de texte
+        top_text = ""  # Initialisation de top_text
+        top = state_data.sort_values(by="Ratio", ascending=False).head(self.classement)
+        for index, row in top.iterrows():
+            words = row['Country'].split()  # Diviser la chaîne en mots
+            half_length = len(words) // 2  # Calculer la moitié du nombre de mots
+            first_half = " ".join(words[:half_length])  # Joindre la première moitié des mots
+            top_text += f"{first_half}: {row['Ratio']:.2f}$\n"          
+        
         # Ajout de la choroplèthe à la carte
         folium.Choropleth(
             geo_data=state_geo,
             name="choropleth",
-            data=state_data,
+            data=top,
             columns=["Country", "Ratio"],
             key_on="feature.properties.name",
             nan_fill_color="White",
@@ -40,15 +53,19 @@ class Donnees:
             legend_name="Ratio argent gagné / joueur",
         ).add_to(m)
 
-        # Ajout du contrôle des calques à la carte
         folium.LayerControl().add_to(m)
-
-        return m
+        
+        # Trie des données selon le ratio
+        state_data.sort_values(by="Ratio", ascending=False, inplace=True)  
+            
+        print(top_text)
+            
+        return  m
     
 # Instanciation d'un objet Donnees avec des valeurs arbitraires
-donnees_esports = Donnees("example", 1000, 500)
+donnees_esports = Esport(2011,10)
 
 # Appel de la méthode ratio() sur l'instance de Donnees
-carte_folium = donnees_esports.ratio()
+carte = donnees_esports.ratio()
 
 carte
