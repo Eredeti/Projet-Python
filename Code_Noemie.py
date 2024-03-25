0cdaf6931227867e7dac362de35bbd71b11c2c59
Nomiel23 <162664861+Nomiel23@users.noreply.github.com>
2024-03-25 17:30:40 +0400

    Update Code_Noemie.py

--- Code_Noemie.py
+++ Code_Noemie.py
@@ -1,1 +1,18 @@
+def marqueur(coordonnees):
+#////////////////////////////////////////////////
+    somme_x, somme_y = 0, 0
+    n = len(coordonnees)
+    for x, y in coordonnees:
+        somme_x += x
+        somme_y += y
 
+    centre = [somme_x / n, somme_y / n]
+    m = folium.Map(centre, zoom_start = 15)
+    folium.Marker( 
+        location = centre,
+        tooltip = "ICI",
+        popup = "je ne sais pas",
+        icon = folium.Icon(icon = "check", color = "red"),
+    ).add_to(m)
+
+    return m
